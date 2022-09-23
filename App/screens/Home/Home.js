50910fb804df512ee44b166ca092f489e285c849
CRIMSON <olueatoworosanwo@gmail.com>
2022-09-23 13:39:33 +0100

    added entering animation to categories

--- App/screens/Home/Home.js
+++ App/screens/Home/Home.js
@@ -5,12 +5,13 @@ import data from "../../../utils/data";
 import { TopBar } from "../../components";
 import { Location } from "../../components/Icons";
 import Cart from "../../components/Icons/Cart";
-import { Center, Text } from "../../components/ui";
+import { Text } from "../../components/ui";
 import { Shadow } from "react-native-shadow-2";
 import theme from "../../../utils/theme";
 import { useEffect, useState } from "react";
 import { ScrollView } from "react-native-gesture-handler";
 import Animated, {
+    FadeInRight,
     interpolateColor,
     useAnimatedStyle,
     useSharedValue,
@@ -27,11 +28,9 @@ function Home() {
     return (
         <View style={styles.container}>
             <SafeAreaView style={styles.container}>
-                <View style={styles.topbarWrapper}>
-                    <TopBar leftIcon={<Location />} rightIcon={<Cart />}>
-                        {data.location}
-                    </TopBar>
-                </View>
+                <TopBar leftIcon={<Location />} rightIcon={<Cart />}>
+                    {data.location}
+                </TopBar>
                 <View>
                     <View style={styles.mainHeader}>
                         <Text styles={styles.mainHeaderText} size={32} weight={500}>
@@ -44,7 +43,7 @@ function Home() {
                     <ScrollView
                         horizontal
                         pagingEnabled
-                        style={styles.categoryFlatList}
+                        style={styles.categoryScrollView}
                         showsHorizontalScrollIndicator={false}
                         snapToInterval={CATEGORY_ITEM_WIDTH + 20}
                     >
@@ -67,8 +66,26 @@ function Home() {
 
 export default Home;
 
-function CategoryItem({ slug, icon, label, selectedCategory, setSelectedCategory }) {
+function CategoryItem({ slug, icon, label, index, selectedCategory, setSelectedCategory }) {
     const animatedColor = useSharedValue(slug === selectedCategory ? 1 : 0);
+
+    const animatedContainerBackgroundStyles = useAnimatedStyle(() => ({
+        backgroundColor: interpolateColor(
+            animatedColor.value,
+            [0, 1],
+            ["#FFFFFF", theme.colors.primary]
+        ),
+    }));
+    const animatedIconContainerBackgroundStyles = useAnimatedStyle(() => ({
+        backgroundColor: interpolateColor(
+            animatedColor.value,
+            [0, 1],
+            [theme.colors[100], "#FFFFFF"]
+        ),
+    }));
+    const animatedTextStyles = useAnimatedStyle(() => ({
+        color: interpolateColor(animatedColor.value, [0, 1], ["#000000", "#FFFFFF"]),
+    }));
 
     useEffect(() => {
         if (slug === selectedCategory) {
@@ -76,31 +93,31 @@ function CategoryItem({ slug, icon, label, selectedCategory, setSelectedCategory
         } else animatedColor.value = withTiming(0);
     }, [selectedCategory]);
 
-    const animatedBackgroundStyles = useAnimatedStyle(() => ({
-        backgroundColor: interpolateColor(
-            animatedColor.value,
-            [0, 1],
-            ["#FFFFFF", theme.colors.primary]
-        ),
-    }));
-    const animatedTextStyles = useAnimatedStyle(() => ({
-        color: interpolateColor(animatedColor.value, [0, 1], ["#000000", "#FFFFFF"]),
-    }));
     return (
-        <Pressable onPress={() => setSelectedCategory(slug)}>
-            <View style={styles.categoryItemWrapper}>
-                <Shadow distance={15} startColor="#F0F0F0" endColor="#FFFFFF00" offset={[0, 8]}>
-                    <Animated.View style={[styles.categoryItem, animatedBackgroundStyles]}>
-                        <Center style={styles.iconWrapper}>
-                            <Image source={icon} style={styles.categoryIcon} />
-                        </Center>
-                        <Animated.Text style={[styles.categoryLabel, animatedTextStyles]}>
-                            {label}
-                        </Animated.Text>
-                    </Animated.View>
-                </Shadow>
-            </View>
-        </Pressable>
+        <Animated.View
+            entering={FadeInRight.delay((index + 10) * 100)
+                .duration(600)
+                .springify()}
+        >
+            <Pressable onPress={() => setSelectedCategory(slug)}>
+                <View style={styles.categoryItemWrapper}>
+                    <Shadow distance={15} startColor="#F0F0F0" endColor="#FFFFFF00" offset={[0, 8]}>
+                        <Animated.View
+                            style={[styles.categoryItem, animatedContainerBackgroundStyles]}
+                        >
+                            <Animated.View
+                                style={[styles.iconWrapper, animatedIconContainerBackgroundStyles]}
+                            >
+                                <Image source={icon} style={styles.categoryIcon} />
+                            </Animated.View>
+                            <Animated.Text style={[styles.categoryLabel, animatedTextStyles]}>
+                                {label}
+                            </Animated.Text>
+                        </Animated.View>
+                    </Shadow>
+                </View>
+            </Pressable>
+        </Animated.View>
     );
 }
 
@@ -124,22 +141,10 @@ function Indicator({ selectedCategory }) {
 
     return (
         <Animated.View
-            style={[
-                {
-                    width: CATEGORY_ITEM_WIDTH + 5,
-                    height: CATEGORY_ITEM_HEIGHT + 5,
-                    borderWidth: 3,
-                    borderColor: theme.colors.primary,
-                    borderRadius: 9999,
-                    alignItems: "center",
-                    position: "absolute",
-                    top: 7.7,
-                    left: 7.7,
-                    zIndex: 20,
-                    // transform: [{ translateX: 50 }],
-                },
-                indicatorAnmatedStyles,
-            ]}
+            entering={FadeInRight.delay(10 * 100)
+                .duration(600)
+                .springify()}
+            style={[styles.indicator, indicatorAnmatedStyles]}
         />
     );
 }
@@ -152,6 +157,7 @@ CategoryItem.propTypes = {
     slug: PropTypes.string,
     icon: PropTypes.number,
     label: PropTypes.string,
+    index: PropTypes.number,
     selectedCategory: PropTypes.string,
     setSelectedCategory: PropTypes.func,
 };
@@ -165,9 +171,6 @@ const styles = StyleSheet.create({
     SafeAreaView: {
         flex: 1,
     },
-    topbarWrapper: {
-        // marginBottom: 30,
-    },
     mainHeader: {
         marginVertical: 30,
         marginBottom: 10,
@@ -175,7 +178,7 @@ const styles = StyleSheet.create({
     mainHeaderText: {
         lineHeight: 36,
     },
-    categoryFlatList: {
+    categoryScrollView: {
         overflow: "visible",
         marginLeft: -10,
         position: "relative",
@@ -184,20 +187,20 @@ const styles = StyleSheet.create({
     categoryItem: {
         width: CATEGORY_ITEM_WIDTH,
         height: CATEGORY_ITEM_HEIGHT,
-        backgroundColor: "#FFFFFF",
         borderRadius: 9999,
         alignItems: "center",
         padding: 5,
         position: "relative",
     },
     iconWrapper: {
-        backgroundColor: theme.colors[100],
         padding: 5,
         borderRadius: 9999,
         width: 45,
         height: 45,
         margin: 0,
         marginBottom: 5,
+        justifyContent: "center",
+        alignItems: "center",
     },
     categoryIcon: {
         width: 20,
@@ -206,5 +209,17 @@ const styles = StyleSheet.create({
     categoryLabel: {
         fontFamily: "poppins-medium",
         fontSize: 12,
+    },
+    indicator: {
+        width: CATEGORY_ITEM_WIDTH + 5,
+        height: CATEGORY_ITEM_HEIGHT + 5,
+        borderWidth: 3,
+        borderColor: theme.colors.primary,
+        borderRadius: 9999,
+        alignItems: "center",
+        position: "absolute",
+        top: 7.7,
+        left: 7.7,
+        zIndex: 20,
     },
 });
